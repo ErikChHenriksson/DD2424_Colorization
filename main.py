5b4eec5d92dd551ac7feee04d46b58dd3ec24589
Ao Song <ao.song@outlook.com>
2021-05-15 00:31:42 +0200

    a running model

--- main.py
+++ main.py
@@ -1,50 +1,45 @@
 from model import Colorizer
+from preprocess import lab_training_loader
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
-import torch
+from torch.autograd import Variable
 import numpy as np
+from torch import split
 
 
-def euclidian_loss(input, target):
-    return torch.sqrt((input - target)**2).sum()/2
+Colorizer = Colorizer()
 
+# No gpu..
+# Colorizer.cuda()
 
-cnn = Colorizer()
-
-optimizer = optim.Adam(cnn.parameters(), lr=0.001)
-criterion = nn.CrossEntropyLoss()
-
-trainset = torch.load('./processed_data/trainset_lab_L_norm.pt')
-labels = torch.load('./processed_data/ab_values5000.pt')
-batch_size = 100
-trainset = trainset[:5000]
-num_batches = int(len(trainset) / batch_size)
+optimizer = optim.Adam(Colorizer.parameters(), lr=0.001)
+criterion = nn.MSELoss()
 
 epochs = 3
+batch_size = 100
 
 for epoch in range(epochs):
     running_loss = 0.0
-    for b in range(num_batches):  # enumerate(trainset, 0):
-        # get the inputs; data is a list of [inputs, labels]
-        b_start = b*batch_size
-        b_end = (b+1)*batch_size
-        inputs, labels = trainset[b_start:b_end], labels[b_start:b_end]
+    for i, data in enumerate(lab_training_loader):
+        # print(data.shape)
+        lab = split(data, [1, 2], dim=1)
+        l = lab[0]
+        ab = lab[1]
 
-        inputs = torch.from_numpy(inputs)
-        labels = torch.from_numpy(np.array(labels))
+        # print(l.shape)
+        # print(ab.shape)
 
-        # zero the parameter gradients
+        l = Variable(l)
+        ab = Variable(ab)
+
+        train = Colorizer(l)
+
         optimizer.zero_grad()
-
-        # forward + backward + optimize
-        outputs = cnn(inputs)
-        loss = euclidian_loss(outputs, labels)
+        loss = criterion(train, ab)
         loss.backward()
         optimizer.step()
 
-        # print statistics
-        running_loss += loss.item()
+        running_loss += (loss % 100)
+    print(f'Running loss is: {running_loss}')
 
-    print(f'loss: {running_loss}')
-    running_loss = 0.0
